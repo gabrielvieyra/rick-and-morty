67a12cbdb1db078c1f448bc06b8ea713d273de35
Gabriel Vieyra <vieyra.gabriel08@gmail.com>
2022-12-08 20:45:06 -0300

    modifico el test characters y pagination

--- cypress/e2e/characters.cy.ts
+++ cypress/e2e/characters.cy.ts
@@ -6,8 +6,16 @@ describe('Characters', () => {
   });
 
   it('Should display a list of twenty characters', () => {
-    cy.intercept('GET', 'https://rickandmortyapi.com/api/character', characters);
+    cy.intercept('GET', 'https://rickandmortyapi.com/api/character', characters).as('characters');
 
     cy.get('[data-cy="character"]').should('have.length', characters.results.length);
+  });
+
+  it('Should show an error when it fails to fetch the list of twenty characters', () => {
+    cy.intercept('GET', 'https://rickandmortyapi.com/api/character', {
+      statusCode: 404,
+    }).as('error');
+
+    cy.contains('Hubo un error y no se pudieron cargar los personajes');
   });
 });
--- cypress/e2e/pagination.cy.ts
+++ cypress/e2e/pagination.cy.ts
@@ -1,14 +1,30 @@
+import characters02 from '../fixtures/characters02.json';
+import characters03 from '../fixtures/characters03.json';
+
 describe('Pagination', () => {
   beforeEach(() => {
     cy.visit('/');
   });
 
-  it('The page should display the following characters', () => {
+  it('The previous button should be disabled', () => {
+    cy.get('[data-type="prev"]')
+      .should('have.css', 'opacity', '0.5')
+      .should('have.css', 'cursor', 'not-allowed');
+  });
+
+  it('The page should display the following twenty characters when press the button next', () => {
+    cy.intercept('GET', 'https://rickandmortyapi.com/api/character?page=2', characters02).as(
+      'characters02'
+    );
+
     cy.get('[data-type="next"]').click();
   });
 
-  it('The page should display the previous characters', () => {
-    cy.get('[data-type="next"]').click();
-    cy.get('[data-type="prev"]').click();
+  it('With the select should  be able to choose to go to a page and see a new list of twenty characters', () => {
+    cy.intercept('GET', 'https://rickandmortyapi.com/api/character?page=9', characters03).as(
+      'characters03'
+    );
+
+    cy.get('[name="goTo"]').select('9');
   });
 });
